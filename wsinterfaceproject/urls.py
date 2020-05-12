feb5660f4fa72a427353ad15cb761ee71b9044dc
brownandblue49 <64905307+brownandblue49@users.noreply.github.com>
2020-05-12 19:07:26 +0530

    Update urls.py

--- wsinterfaceproject/urls.py
+++ wsinterfaceproject/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     url(r'^$', views.home, name="home"), 
   # url(r'^output', views.output,name="script"),
     url(r'^external', views.external , name="script" ),
-    url(r'^index', views.index , name="script" ),
+   # url(r'^index', views.index , name="script" ),
     path('EPM/', views.EPMFileUpload, name="EPMFileUpload"),
     path('Index/', views.index)
        
